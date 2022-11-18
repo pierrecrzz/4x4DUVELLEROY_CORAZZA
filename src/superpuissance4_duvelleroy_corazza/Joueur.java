0fce81b71ee1ecb086fd4ea13ef2bf020ef688aa
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-11-18 15:28:14 +0100

    initialisation des classes

---
+++ src/superpuissance4_duvelleroy_corazza/Joueur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superpuissance4_duvelleroy_corazza;
+
+/**
+ *
+ * @author valen
+ */
+public class Joueur {
+    
+    String nom;
+    String couleur;
+    
+public Joueur (String n, String c){
+    nom = n;
+    couleur = c;           
+}
+}
--- src/superpuissance4_duvelleroy_corazza/SuperPuissance4_DUVELLEROY_CORAZZA.java
+++ src/superpuissance4_duvelleroy_corazza/SuperPuissance4_DUVELLEROY_CORAZZA.java
@@ -15,6 +15,7 @@ public class SuperPuissance4_DUVELLEROY_CORAZZA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
