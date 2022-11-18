04bcea8b761b336c8a53c569ca40cee1952f0412
coraz <coraz@LAPTOP-0SM82EUU.education.lakanal.epf.fr>
2022-11-18 14:38:52 +0100

    Debut projet

---
+++ src/superpuissance4_duvelleroy_corazza/SuperPuissance4_DUVELLEROY_CORAZZA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package superpuissance4_duvelleroy_corazza;
+
+/**
+ *
+ * @author coraz
+ */
+public class SuperPuissance4_DUVELLEROY_CORAZZA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
