9c483ffbf343c07eb58aeeffbd119b9ab140dc1e
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-11-28 18:56:18 +0100

    continuation des classes CelluleDeGrille et Jetons + Joueur

--- src/superpuissance4_duvelleroy_corazza/CelluledeGrille.java
+++ src/superpuissance4_duvelleroy_corazza/CelluledeGrille.java
@@ -10,4 +10,51 @@ package superpuissance4_duvelleroy_corazza;
  */
 public class CelluledeGrille {
     
+private Jeton JetonCourant;
+private boolean AvoirTrouNoir;
+private boolean avoirDesintegrateur;
+        
+    Jeton JetonCourant= null;
+    AvoirTrouNoir= false;
+    AvoirDesintegrateur= false;
+    // Par défaut une cellule ne possède ni jeton (= à null), ni trou noir (= à false), ni désintegrateur (= à false)
+    
+        
+        CelluleDeGrille(){
+        if (jetonCourant==null){
+        presenceJeton()==false;
+        return false;
+        } else {
+    return true
+            
+            
+            
+       
+
+
+
+
+
+                
+                placerTrouNoir()
+                       avoirTrouNoir=true;
+                supprimerTrouNoir()
+                       avoirTrouNoir=false;
+                       
+                       
+                       
+                       if (avoirTrouNoir() == true){
+                               presenceTrouNoir()=true;
+                       }else {
+                           presenceTrouNoir()=false;
+                           
+                               
+                    
+                }
+                       
+
+}
+
+}
+    
 }
