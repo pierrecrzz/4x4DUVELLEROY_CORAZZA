85d819b4fdd1eb868c87c16f9ba4bbdb424165ee
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-12-05 19:51:10 +0100

    On a eu un problème avec l'ordinateur de Pierre, on ne peut plus travailler en partagé, il m'a donc envoyé notre code que je commit maintenant

--- src/superpuissance4_duvelleroy_corazza/Joueur.java
+++ src/superpuissance4_duvelleroy_corazza/Joueur.java
@@ -4,17 +4,51 @@
  */
 package superpuissance4_duvelleroy_corazza;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author valen
  */
 public class Joueur {
-    
-    String nom;
-    String couleur;
-    
-public Joueur (String n, String c){
-    nom = n;
-    couleur = c;           
-}
-}
+
+        private String nom;
+        private String couleur;
+        private ArrayList<Jeton> reserveJetons=new ArrayList();
+        private int nombreDesintegrateurs;
+
+   
+    public Joueur (String nom){
+       this.nom=nom ;
+       this.nombreDesintegrateurs= 0;
+    }
+   
+    public void affecterCouleur(String couleur){
+        this.couleur=couleur;
+    }
+    public String lireCouleur(){
+        return couleur;
+    }
+   
+    public int nombreJetons (){
+        return reserveJetons.size();
+    }
+   
+    public void ajouterJetons(Jeton unJeton ){
+        reserveJetons.add(unJeton);
+    }
+   
+    public Jeton jouerJeton(){
+        Jeton elvJeton = reserveJetons.remove(0);    
+        return elvJeton;
+    }
+   
+    public void obtenirDesintegrateur(){
+        nombreDesintegrateurs+=1;
+    }
+     
+    public void utiliserDesintegrateur(){
+        nombreDesintegrateurs-=1;
+    }
+       
+    }   
