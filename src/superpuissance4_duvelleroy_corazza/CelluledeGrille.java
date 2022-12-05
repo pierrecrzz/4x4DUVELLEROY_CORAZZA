7546737cce595f015d02587c531ebcb50b8b85b2
valen <valen@LAPTOP-PM0POMS3.education.lakanal.epf.fr>
2022-12-05 19:56:01 +0100

    finition de la CelluleDeGrille

--- src/superpuissance4_duvelleroy_corazza/CelluledeGrille.java
+++ src/superpuissance4_duvelleroy_corazza/CelluledeGrille.java
@@ -15,77 +15,106 @@ public class CelluledeGrille {
         this.AvoirTrouNoir = false;
         this.avoirDesintegrateur = false;
     }
-    
-private Jeton JetonCourant;
-private boolean AvoirTrouNoir;
-private boolean avoirDesintegrateur;
-        
-
-public boolean presenceJeton(){  
-    if (JetonCourant==null){
-        return false;
-    }
-    else {
-        return true;
-    }
-}      
-      
-}
-        
-  public String lireCouleurDuJeton(){
-          if (JetonCourant!= null){
-              if (couleur==R)
-          }
-          
-          
-
-
-
-
-
-
-
-Jeton JetonCourant= null;
-    AvoirTrouNoir= false;
-    AvoirDesintegrateur= false;
-    // Par défaut une cellule ne possède ni jeton (= à null), ni trou noir (= à false), ni désintegrateur (= à false)
-    
-        
-        CelluleDeGrille(){
-        if (jetonCourant==null){
-        presenceJeton()==false;
-        return false;
-        } else {
-    return true
-            
-            
-            
+   
+    private Jeton JetonCourant;
+    private boolean AvoirTrouNoir;
+    private boolean avoirDesintegrateur;
        
-
-
-
-
-
-                
-                placerTrouNoir()
-                       avoirTrouNoir=true;
-                supprimerTrouNoir()
-                       avoirTrouNoir=false;
-                       
-                       
-                       
-                       if (avoirTrouNoir() == true){
-                               presenceTrouNoir()=true;
-                       }else {
-                           presenceTrouNoir()=false;
-                           
-                               
-                    
-                }
-                       
+ 
+   
+    public boolean affecterJeton (Jeton J) {
+        if (JetonCourant == null){
+            JetonCourant = J ;
+            return true;
+        } else {
+           return false ;
+}
+}
+    public String lireCouleurDuJeton(){
+        return JetonCourant.Couleur ;
+  }
+       
+    public boolean PlacerTrouNoir(boolean AvoirTrouNoir) {
+        if (AvoirTrouNoir==false) {
+            AvoirTrouNoir= true ;
+            return true ;
+        }
+        else{
+            return false;
+        }
 
 }
+   
+    public boolean presenceTrouNoir(boolean AvoirTrouNoir) {
+        if (AvoirTrouNoir==true){
+            return true;
+        }
+        else{
+            return false ;            
+        }
+    }
+ 
+   
+     
+     
+    public Jeton recupererJeton () {
+        return JetonCourant ;
+    }
+   
+    public boolean supprimerJeton(int JetonCourant) {
+        if (JetonCourant!=0) {
+            JetonCourant = 0;
+            return true;
+                    }
+        else {
+            return false;
+                   
+        }
+    }
+    public boolean presenceDesintegrateur (boolean avoirDesintegrateur) {
+        if (avoirDesintegrateur==true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+   
+    public boolean placerDesintegrateur (boolean avoirDesintegrateur) {
+        if (avoirDesintegrateur==false){
+            avoirDesintegrateur=true;
+            return true;
+        } else {
+            return false;
+        }              
+    }
+   
+    public boolean supprimerDesintegrateur (boolean avoirDesintegrateur){
+        if (avoirDesintegrateur == true){
+            avoirDesintegrateur= false;
+            return true;
+        }else{
+            return false;
+        }
+           
+}
+   
+    public boolean activerTrouNoir (boolean AvoirTrouNoir){
+        if (AvoirTrouNoir==true){
+            AvoirTrouNoir=false;
+            return true;
+        }else{
+            return false;
+        }
+    }
+   
+    public boolean supprimerTrouNoir (boolean AvoirTrouNoir){
+        if (AvoirTrouNoir==true){
+            AvoirTrouNoir=false;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
 
-}
-    
-}
